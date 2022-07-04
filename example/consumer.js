d8011ab12f1d81168f962d33871bbc3e78545751
sinkhaha <1468709106@qq.com>
2022-07-04 14:47:28 +0800

    fix

--- example/consumer.js
+++ example/consumer.js
@@ -12,7 +12,7 @@ worker.register({
      */
     testMethod: function (params, callback) {
         try {
-            console.log('处理消息', params);
+            console.log('处理消息', params, typeof params);
             // callback第1个参数是err，第2个参数是结果，会把result更新到数据库的消息中
             callback(null, params);
         } catch (err) {
